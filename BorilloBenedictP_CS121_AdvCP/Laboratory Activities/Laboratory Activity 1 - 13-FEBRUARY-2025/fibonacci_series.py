136f3d1de2641695b37c3fffea9fb11ed347b7b0
Borillo, Benedict <benedictborillo@gmail.com>
2025-02-13 11:59:26 +0800

    fibonacci_series.py

--- BorilloBenedictP_CS121_AdvCP/Laboratory Activities/Laboratory Activity 1 - 13-FEBRUARY-2025/fibonacci_series.py
+++ BorilloBenedictP_CS121_AdvCP/Laboratory Activities/Laboratory Activity 1 - 13-FEBRUARY-2025/fibonacci_series.py
@@ -1,10 +1,10 @@
-num = int(input("Enter a number: "))
+num = int(input("Enter the number of terms: "))
 first = 0
 second = 1
 temp = 0
 
 print("Fibonacci Series:", end=" ")
-for i in range(num+2):
+for i in range(num):
     print(first, end=" ")
     temp = second
     second = first + second
