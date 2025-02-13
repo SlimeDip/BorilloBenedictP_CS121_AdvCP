a88077955f3d5b5da07b12921504cdce2e32e26b
Borillo, Benedict <benedictborillo@gmail.com>
2025-02-13 11:57:59 +0800

    fibonacci_series.py

--- BorilloBenedictP_CS121_AdvCP/Laboratory Activities/Laboratory Activity 1 - 13-FEBRUARY-2025/fibonacci_series.py
+++ BorilloBenedictP_CS121_AdvCP/Laboratory Activities/Laboratory Activity 1 - 13-FEBRUARY-2025/fibonacci_series.py
@@ -3,16 +3,9 @@ first = 0
 second = 1
 temp = 0
 
-if num == 1:
-    print("Fibonacci Series: 0")
-elif num == 2:
-    print("Fibonacci Series: 0 1")
-elif num >= 3:
-    print("Fibonacci Series: 0 1", end=" ")
-    for i in range(num-2):
-        print(first + second, end=" ")
-        temp = second
-        second = first + second
-        first = temp
-else:
-    print("Invalid number")
+print("Fibonacci Series:", end=" ")
+for i in range(num+2):
+    print(first, end=" ")
+    temp = second
+    second = first + second
+    first = temp
